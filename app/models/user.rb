808e9471ae598bc712986845277326b8a93ad205
Jason Guzik <jguzik@gmail.com>
2015-06-19 17:54:44 -0400

    Add initial associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :created_surveys, foreign_key: :creator_id
+  has_many :taken_surveys
+  has_many :surveys, through: :taken_surveys
+  has_many :users_selections
+  has_many :choices, through: :users_selections
 end
--- app/models/user_selection.rb
+++ app/models/user_selection.rb
@@ -1,3 +1,4 @@
 class UserSelection < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :choice
+  belongs_to :user
 end

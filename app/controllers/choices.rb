9e8ce5205e5545365e2b7d969cfe70c60a9c94b9
JMC11 <JMC11614792@gmail.com>
2015-06-20 16:48:26 -0400

    creating a choice route added

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -3,3 +3,12 @@ get '/surveys/:id/questions/:question_id/choices/new' do
   @question = Question.find_by(id: params[:question_id])
   erb :'/choices/new'
 end
+
+post '/surveys/:id/questions/:question_id/choices' do
+  choice = Choice.new(params[:choice])
+  return [500, 'Invalid Choice'] unless choice.save
+  question = Question.find_by(id: params[:question_id])
+  survey = Survey.find_by(id: params[:id])
+  question.choices << choice
+  redirect "/surveys/#{survey.id}"
+end

e8bf1b9bf5a85280bb5a2a24b54ebe2b83cfa187
JMC11 <JMC11614792@gmail.com>
2015-06-20 14:30:07 -0400

    added skeleton routes

---
+++ app/controllers/questions.rb
@@ -0,0 +1,19 @@
+get '/surveys/:id/questions/new' do
+
+end
+
+post '/surveys/:id/questions' do
+
+end
+
+get '/surveys/:id/questions/:question_id/edit' do
+
+end
+
+put '/surveys/:id/questions/:question_id' do
+
+end
+
+delete '/surveys/:id/questions/:question_id' do
+
+end

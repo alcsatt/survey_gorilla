296ff4b270cc6045a23dc6c75f56017e4072ca78
Alex Satterly <alcsatt@gmail.com>
2015-06-20 11:50:46 -0400

    Stub out routes for CRUDing a survey

---
+++ app/controllers/surveys.rb
@@ -0,0 +1,34 @@
+# Index
+get '/surveys' do
+
+end
+
+# New
+get '/surveys/new' do
+
+end
+
+# Show
+get '/surveys/:title' do
+
+end
+
+# Create
+post '/surveys' do
+
+end
+
+# Edit
+get '/surveys/:id/edit' do
+
+end
+
+# Update
+patch '/surveys/:id' do
+
+end
+
+# Destroy
+delete '/surveys/:id' do
+
+end
